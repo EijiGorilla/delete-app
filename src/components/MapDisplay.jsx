ddfe378e6bd022caffbeda2ee879e58d32c91877
EijiGorilla <emasuz@hotmail.com>
2026-01-06 07:29:40 +0800

    updated

---
+++ src/components/MapDisplay.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "../index.css";
+
+import "@arcgis/map-components/dist/components/arcgis-scene";
+import "@arcgis/map-components/components/arcgis-elevation-profile";
+
+// 2 D <-> 3D
+// https://developers.arcgis.com/javascript/latest/sample-code/views-switch-2d-3d/
+
+export default function MapDisplay() {
+  const [mapView, setMapView] = useState();
+  const arcgisMap = document.querySelector("arcgis-scene");
+
+  return (
+    <arcgis-scene
+      basemap="dark-gray-vector"
+      viewingMode="local"
+      ground="world-elevation"
+      id="arcgis-scene-id"
+      onarcgisViewReadyChange={(event) => {
+        setMapView(event.target);
+      }}
+    >
+      <arcgis-elevation-profile
+        slot="bottom-right"
+        unit="millimeters"
+      ></arcgis-elevation-profile>
+    </arcgis-scene>
+  );
+}
