f152fc0aed726646ab5b8ade5b59876ce52d8f17
EijiGorilla <emasuz@hotmail.com>
2026-01-06 08:36:29 +0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
 import "./App.css";
 import MapDisplay from "./components/MapDisplay";
-
+// Check out this page for vite app with elevaton profile: https://www.youtube.com/watch?v=SeFUjIg_BaM
 function App() {
-  // Catch failed dynamic imports
-  window.addEventListener("unhandledrejection", (event) => {
-    const message = event.reason?.message || "";
-
-    if (message.includes("Failed to fetch dynamically imported module")) {
-      event.preventDefault(); // Hide error from user
-      window.location.reload(); // Silent refresh
-    }
-  });
-
-  // Catch other asset loading errors
-  window.addEventListener("error", (event) => {
-    const message = event.message || "";
-
-    if (
-      message.includes("Loading chunk") ||
-      message.includes("ChunkLoadError")
-    ) {
-      event.preventDefault();
-      window.location.reload();
-    }
-  });
   return (
     <>
       <MapDisplay />
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
-// https://vite.dev/config/
+
 export default defineConfig({
   // plugins: [react()],
-  plugins: [
-    {
-      name: "generate-version",
-      buildStart() {
-        const version = {
-          version: process.env.npm_package_version,
-          buildTime: new Date().toISOString(),
-        };
-
-        fs.writeFileSync("public/version.json", JSON.stringify(version));
-      },
-    },
-    react(),
-  ],
-  base: "",
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    open: true,
+  },
+  base: "/delete-app/",
 });
