014105404c41bacce8c107f0eaa340da1cdafaa0
EijiGorilla <emasuz@hotmail.com>
2026-01-06 10:13:45 +0800

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   // plugins: [react()],
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
+  // build: {
+  //   outDir: "dist",
+  // },
   server: {
     open: true,
   },
