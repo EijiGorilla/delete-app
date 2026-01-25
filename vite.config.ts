79cc278c02af0ec3889062872ca734583eecc536
EijiGorilla <emasuz@hotmail.com>
2026-01-25 09:44:20 +0800

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  // plugins: [react()],
-  plugins: [react()],
-  // build: {
-  //   outDir: "dist",
-  // },
-  server: {
-    open: true,
-  },
-  base: "/delete-app/",
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     open: true,
+//   },
+//   base: "/delete-app/",
+// });
+
+export default defineConfig(({ command }) => {
+  if (command === "build") {
+    return {
+      plugins: [react()],
+      base: "/delete-app/", // Use this path for production builds
+      // ...
+    };
+  }
+  return {
+    plugins: [react()],
+    base: "/", // Use default path for development server
+    // ...
+  };
 });
